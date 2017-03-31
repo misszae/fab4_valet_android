fd5d8fae495896e848b6d9d562479c46f9c0bbc3
Luke <verdant.luke@gmail.com>
2017-03-31 12:26:20 -0400

    adding mqtt

--- src/main/java/fab4/myapplication/MainActivity.java
+++ src/main/java/fab4/myapplication/MainActivity.java
@@ -12,9 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import messaging.*;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    MQTT msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        msg = new MQTT(getApplicationContext());
+
+        msg.Connect();
     }
 
     @Override
@@ -94,6 +100,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void btnFetch_OnClick(View view) {
-
+        msg.publishMessage();
     }
 }
